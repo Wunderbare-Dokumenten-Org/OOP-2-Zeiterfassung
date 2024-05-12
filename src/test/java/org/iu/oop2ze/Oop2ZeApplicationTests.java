95172c4f03efbc149c42f1763d4771208919cf54
Jxstxs <juleb0202@gmail.com>
2024-05-12 17:04:15 +0200

    feat: init des spring boot programmes

---
+++ src/test/java/org/iu/oop2ze/Oop2ZeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iu.oop2ze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oop2ZeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
