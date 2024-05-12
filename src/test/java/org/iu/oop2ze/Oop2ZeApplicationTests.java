3d214e4887342d7aa9129dde8c40c3ee6c266fa1
Jxstxs <juleb0202@gmail.com>
2024-05-12 18:21:15 +0200

    fmt(tests): richtige formatierung verwendet

--- src/test/java/org/iu/oop2ze/Oop2ZeApplicationTests.java
+++ src/test/java/org/iu/oop2ze/Oop2ZeApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Oop2ZeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
