2390ed488348086e0e9d93700c304bc31d63f396
Jxstxs <juleb0202@gmail.com>
2024-05-13 16:37:46 +0200

    fmt(db): repositories angepasst

--- src/main/java/org/iu/oop2ze/core/database/repositories/PersonRepository.java
+++ src/main/java/org/iu/oop2ze/core/database/repositories/PersonRepository.java
@@ -3,5 +3,4 @@ package org.iu.oop2ze.core.database.repositories;
 import org.iu.oop2ze.core.database.models.Person;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonRepository extends CrudRepository<Person, Long> {
-}
+public interface PersonRepository extends CrudRepository<Person, Long> {}
